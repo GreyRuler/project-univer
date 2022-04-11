585a015f406c9cae0dac2f860400270f7dc45f8f
GreyRuler <daniil_vasilenko757@mail.ru>
2022-04-11 17:51:01 +0300

    v 2.0

--- square.py
+++ square.py
@@ -8,19 +8,15 @@ class Point:
         self.x = x
         self.y = y
 
-    @property
-    def get_coords(self):
-        return self.x, self.y
 
-
-def generate_point(count: int) -> List[Point]:
+def generate_point(count: int, of: int, to: int) -> List[Point]:
     points: List[Point] = []
     for i in range(count):
-        points.append(Point(randrange(0, 100), randrange(0, 100)))
+        points.append(Point(randrange(of, to), randrange(of, to)))
     return points
 
 
-def calc_vector_coordinates(point_b: Point, point_a: Point):
+def vector_coordinates(point_a: Point, point_b: Point):
     return point_b.x - point_a.x, point_b.y - point_a.y
 
 
@@ -28,50 +24,48 @@ def composition_vector(point_a: Point, point_b: Point):
     return point_a.x * point_b.y - point_a.y * point_b.x
 
 
-def calculation_area(point_a: Point, point_b: Point, point_c: Point, point_d: Point):
+def area_square(point_a: Point, point_b: Point, point_c: Point, point_d: Point):
     return sum((composition_vector(point_a, point_b),
                 composition_vector(point_b, point_c),
                 composition_vector(point_c, point_d),
-                composition_vector(point_d, point_a)))
+                composition_vector(point_d, point_a))) / 2
 
 
-def calculating_area_square(points):
+def square(points):
     eps = 1e-10
     min_area = 0
     max_area = 0
-    coords_min_area = ()
-    coords_max_area = ()
+    index_points_min_area = ()
+    index_points_max_area = ()
 
     for a in range(len(points)):
-        for b in range(len(points)):
-            if b != a:
-                for c in range(len(points)):
-                    if c != a and c != b:
-                        vector_a = calc_vector_coordinates(points[b], points[a])
-                        vector_b = calc_vector_coordinates(points[c], points[b])
-                        if vector_a[0] == vector_b[1] and abs(vector_a[1]) == vector_b[0]:
-                            for d in range(len(points)):
-                                vector_c = calc_vector_coordinates(points[d], points[c])
-                                if abs(vector_a[0]) == vector_c[0] and abs(vector_a[1]) == vector_c[1]:
-                                    area = calculation_area(points[a], points[b], points[c], points[d])
+        for b in range(a, len(points)):
+            for c in range(b, len(points)):
+                vector_a = vector_coordinates(points[a], points[b])
+                vector_b = vector_coordinates(points[b], points[c])
+                if vector_a[0] == vector_b[1] and abs(vector_a[1]) == vector_b[0]:
+                    for d in range(c, len(points)):
+                        vector_c = vector_coordinates(points[c], points[d])
+                        vector_d = vector_coordinates(points[d], points[a])
+                        if abs(vector_a[0]) == vector_c[0] and abs(vector_a[1]) == vector_c[1] and abs(vector_a[0]) == vector_d[1] and vector_a[1] == vector_d[0]:
+                            area = area_square(points[a], points[b], points[c], points[d])
 
-                                    if area > max_area:
-                                        max_area = area
-                                        coords_max_area = (a, b, c, d)
+                            if area > max_area:
+                                max_area = area
+                                index_points_max_area = (a, b, c, d)
 
-                                    if area > eps and (min_area - area > eps or min_area < eps):
-                                        min_area = area
-                                        coords_min_area = (a, b, c, d)
+                            if area > eps and (min_area - area > eps or min_area < eps):
+                                min_area = area
+                                index_points_min_area = (a, b, c, d)
 
-    return min_area, coords_min_area, max_area, coords_max_area
+    return min_area, index_points_min_area, max_area, index_points_max_area
 
 
 def main():
+    points = generate_point(count=10, of=0, to=5)
+
     start_time = time.time()
-
-    points = generate_point(count=100)
-    min_area, coords_min_area, max_area, coords_max_area = calculating_area_square(points)
-
+    min_area, index_points_min_area, max_area, index_points_max_area = square(points)
     print("--- %s seconds ---" % (time.time() - start_time))
 
     if min_area == 0 and max_area == 0:
@@ -79,16 +73,16 @@ def main():
     else:
         print(f"Площадь наибольшего квадрата равна: {max_area}")
         print(f"Его координаты:"
-              f" {points[coords_max_area[0]].x, points[coords_max_area[0]].y},"
-              f" {points[coords_max_area[1]].x, points[coords_max_area[1]].y},"
-              f" {points[coords_max_area[2]].x, points[coords_max_area[2]].y},"
-              f" {points[coords_max_area[3]].x, points[coords_max_area[3]].y}")
+              f" {points[index_points_max_area[0]].x, points[index_points_max_area[0]].y},"
+              f" {points[index_points_max_area[1]].x, points[index_points_max_area[1]].y},"
+              f" {points[index_points_max_area[2]].x, points[index_points_max_area[2]].y},"
+              f" {points[index_points_max_area[3]].x, points[index_points_max_area[3]].y}")
         print(f"Площадь наименьшего квадрата равна: {min_area}")
         print(f"Его координаты:"
-              f" {points[coords_min_area[0]].x, points[coords_min_area[0]].y},"
-              f" {points[coords_min_area[1]].x, points[coords_min_area[1]].y},"
-              f" {points[coords_min_area[2]].x, points[coords_min_area[2]].y},"
-              f" {points[coords_min_area[3]].x, points[coords_min_area[3]].y}")
+              f" {points[index_points_min_area[0]].x, points[index_points_min_area[0]].y},"
+              f" {points[index_points_min_area[1]].x, points[index_points_min_area[1]].y},"
+              f" {points[index_points_min_area[2]].x, points[index_points_min_area[2]].y},"
+              f" {points[index_points_min_area[3]].x, points[index_points_min_area[3]].y}")
 
 
 if __name__ == '__main__':
--- triangle.py
+++ triangle.py
@@ -1,4 +1,5 @@
-from random import uniform
+import time
+from random import randrange
 from typing import List
 
 
@@ -8,64 +9,73 @@ class Point:
         self.y = y
 
 
-def generate_point(count: int) -> List[Point]:
+def generate_point(count: int, of: int, to: int) -> List[Point]:
     points: List[Point] = []
     for i in range(count):
-        points.append(Point(uniform(0, 5), uniform(0, 5)))
+        points.append(Point(randrange(of, to), randrange(of, to)))
     return points
 
 
-def calc_vector_coordinates(point_b: Point, point_a: Point):
+def vector_coordinates(point_a: Point, point_b: Point):
     return point_b.x - point_a.x, point_b.y - point_a.y
 
 
-def calculating_area_triangle(points: List[Point]) -> tuple[int, tuple[int, int, int], int, tuple[int, int, int]]:
+def area_triangle(vector_a: tuple[float, float], vector_b: tuple[float, float]):
+    return abs(vector_a[0] * vector_b[1] - vector_a[1] * vector_b[0]) / 2
+
+
+def triangle(points: List[Point]) -> tuple[int, tuple[int, int, int], int, tuple[int, int, int]]:
     eps = 1e-10
     min_area = 0
     max_area = 0
-    coords_min_area = ()
-    coords_max_area = ()
+    index_points_min_area = ()
+    index_points_max_area = ()
 
     for a in range(len(points)):
-        for b in range(len(points)):
-            vector_a = calc_vector_coordinates(points[b], points[a])
-            for c in range(len(points)):
-                vector_b = calc_vector_coordinates(points[c], points[b])
-                area = abs(vector_a[0] * vector_b[1] - vector_a[1] * vector_b[0]) / 2
+        for b in range(a, len(points)):
+            vector_a = vector_coordinates(points[a], points[b])
+            for c in range(b, len(points)):
+                vector_b = vector_coordinates(points[b], points[c])
+                area = area_triangle(vector_a, vector_b)
 
                 if area > max_area:
                     max_area = area
-                    coords_max_area = (a, b, c)
+                    index_points_max_area = (a, b, c)
 
                 if area > eps and (min_area - area > eps or min_area < eps):
                     min_area = area
-                    coords_min_area = (a, b, c)
+                    index_points_min_area = (a, b, c)
 
-    return min_area, coords_min_area, max_area, coords_max_area
+    return min_area, index_points_min_area, max_area, index_points_max_area
 
 
 def main():
-    points = generate_point(count=10)
-    min_area, coords_min_area, max_area, coords_max_area = calculating_area_triangle(points)
-    for i in range(len(coords_min_area)):
-        print(points[coords_min_area[i]].x)
-        print(points[coords_min_area[i]].y)
-    coords_x_max = [points[coords_max_area[i]].x for i in range(len(coords_max_area))]
-    coords_y_max = [points[coords_max_area[i]].y for i in range(len(coords_max_area))]
-    coords_x_max.append(points[coords_max_area[0]].x)
-    coords_y_max.append(points[coords_max_area[0]].y)
+    points = generate_point(count=100, of=-100, to=100)
+
+    start_time = time.time()
+    min_area, index_points_min_area, max_area, index_points_max_area = triangle(points)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    for i in range(len(index_points_min_area)):
+        print(points[index_points_min_area[i]].x)
+        print(points[index_points_min_area[i]].y)
+
+    coords_x_max = [points[index_points_max_area[i]].x for i in range(len(index_points_max_area))]
+    coords_y_max = [points[index_points_max_area[i]].y for i in range(len(index_points_max_area))]
+    coords_x_max.append(points[index_points_max_area[0]].x)
+    coords_y_max.append(points[index_points_max_area[0]].y)
 
     print(f"Площадь наименьшего треугольника равна: {min_area}")
     print(f"Его координаты:"
-          f" {points[coords_min_area[0]].x, points[coords_min_area[0]].y},"
-          f" {points[coords_min_area[1]].x, points[coords_min_area[1]].y},"
-          f" {points[coords_min_area[2]].x, points[coords_min_area[2]].y}")
+          f" {points[index_points_min_area[0]].x, points[index_points_min_area[0]].y},"
+          f" {points[index_points_min_area[1]].x, points[index_points_min_area[1]].y},"
+          f" {points[index_points_min_area[2]].x, points[index_points_min_area[2]].y}")
 
     print(f"Площадь наибольшего треугольника равна: {max_area}")
     print(f"Его координаты:"
-          f" {points[coords_max_area[0]].x, points[coords_max_area[0]].y},"
-          f" {points[coords_max_area[1]].x, points[coords_max_area[1]].y},"
-          f" {points[coords_max_area[2]].x, points[coords_max_area[2]].y}")
+          f" {points[index_points_max_area[0]].x, points[index_points_max_area[0]].y},"
+          f" {points[index_points_max_area[1]].x, points[index_points_max_area[1]].y},"
+          f" {points[index_points_max_area[2]].x, points[index_points_max_area[2]].y}")
 
 
 if __name__ == '__main__':
